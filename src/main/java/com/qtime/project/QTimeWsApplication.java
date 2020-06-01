28992e516ddcecd266b5410700142f32801e6a89
unknown <vivekjha4u2@gmail.com>
2020-06-01 15:43:08 +0530

    initial commit

---
+++ src/main/java/com/qtime/project/QTimeWsApplication.java
@@ -0,0 +1,13 @@
+package com.qtime.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QTimeWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QTimeWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qtime/project/QTimeWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qtime.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QTimeWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
